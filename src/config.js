89ed5c7f32f0fedff4469af5aeaa020e94dcc577
avery011 <avery011@gmail.com>
2024-01-18 16:05:40 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.origin === "localhost" ? "https://mavery-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+export const API_URL = window.location.origin === "https://mavery-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://mavery-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
 console.log(
     "API_URL :",
     API_URL
